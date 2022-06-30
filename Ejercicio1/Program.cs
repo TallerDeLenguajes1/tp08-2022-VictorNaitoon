740ef755e7534c6948a7a75015843e870296aae6
Victor Molina <Victormolinalvp@gmail.com>
2022-06-30 10:46:16 -0300

    Tp Terminado

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la direccion de su archivo: ");
+        string DireccionCarpeta = Console.ReadLine();
+
+        List<string> ListaArchivos = new List<string>();
+        if (Directory.Exists(DireccionCarpeta))
+        {
+            ListaArchivos = Directory.GetFiles(DireccionCarpeta).ToList();
+        } else {
+            Console.WriteLine("No existe la tal archivo");
+        }
+        foreach (string ArchivoX in ListaArchivos)
+        {
+            Console.WriteLine(ArchivoX);
+        }
+
+        string NombreDelArchivoCSV = DireccionCarpeta + @"\index.csv";
+        if (!File.Exists(NombreDelArchivoCSV))
+        {
+            File.Create(NombreDelArchivoCSV);
+        }
+
+        using (var streamWriter = new StreamWriter(NombreDelArchivoCSV))
+        {
+            int i = 0;
+            foreach (var ArchivoY in ListaArchivos)
+            {
+                Console.WriteLine(ArchivoY);
+                streamWriter.WriteLine(i++ +";");
+                streamWriter.WriteLine(Path.GetFileNameWithoutExtension(NombreDelArchivoCSV)+";"+Path.GetExtension(NombreDelArchivoCSV));
+            }
+        }
+    }
+}
